92ff598150c30ff875638a930b9aec1540383f77
Geevan Patel <geevan.peter.patel@gmail.com>
2019-02-28 21:23:52 +0000

    Keeping Fit - Completed

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -43,3 +43,21 @@ describe('Initial fitness', () => {
     expect(pet.fitness).toEqual(10);
   });
 });
+
+describe('walk', () => {
+  it('increases fitness by 4', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 4;
+    pet.walk();
+    expect(pet.fitness).toEqual(8);
+  });
+});
+
+describe('walk', () => {
+  it('increases fitness by to a maximum of 10', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 8;
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  });
+});
